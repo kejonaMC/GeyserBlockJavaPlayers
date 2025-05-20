45476583dec520c576bb1ec96cc13d2744bb3db1
Jens Collaert <jenscollaert@hotmail.com>
2025-05-20 03:18:36 +0200

    Add build file, update Geyser api usage and update maven shading version

--- common/src/main/java/com/github/camotoy/geyserblockjavaplayers/common/GeyserJavaPlayerChecker.java
+++ common/src/main/java/com/github/camotoy/geyserblockjavaplayers/common/GeyserJavaPlayerChecker.java
@@ -1,19 +1,18 @@
 package com.github.camotoy.geyserblockjavaplayers.common;
 
-import org.geysermc.geyser.GeyserImpl;
+import org.geysermc.geyser.api.GeyserApi;
+import org.geysermc.geyser.api.connection.GeyserConnection;
 
-import java.util.Objects;
 import java.util.UUID;
 
 public class GeyserJavaPlayerChecker implements JavaPlayerChecker {
-    private final GeyserImpl connector;
-
-    public GeyserJavaPlayerChecker() {
-        this.connector = Objects.requireNonNull(GeyserImpl.getInstance());
-    }
-
     @Override
     public boolean isBedrockPlayer(UUID uuid) {
-        return this.connector.connectionByUuid(uuid) != null;
+        if (GeyserApi.api() == null) {
+            return false;
+        }
+
+        GeyserConnection connection = GeyserApi.api().connectionByUuid(uuid);
+        return connection != null;
     }
 }
